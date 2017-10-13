2a23dc9f0ea6ff27bda2bf7e172df8e21a9b5005
fabwa <fabian.wahle@gmail.com>
2017-10-13 17:49:24 +0200

    Update HelloWorld.java
    Main needs to be capital to be caught by neoj

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -5,7 +5,7 @@ import org.neo.smartcontract.framework.services.neo.Storage;
 
 public class HelloWorld extends SmartContract{
 
-  public static void main(String[] args){
+  public static void Main(String[] args){
     Storage.put(Storage.currentContext(), "Greeting to the World", "Hello World!");
   }
 
