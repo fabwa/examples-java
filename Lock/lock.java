c0188868898aa06a4840cbb3acf2e14c71d8d68c
fabwa <fabian.wahle@gmail.com>
2017-10-13 23:06:54 +0200

    Update lock.java

--- Lock/lock.java
+++ Lock/lock.java
@@ -1,5 +1,3 @@
-package examples_neo;
-
 import org.neo.smartcontract.framework.SmartContract;
 import org.neo.smartcontract.framework.services.neo.Blockchain;
 import org.neo.smartcontract.framework.services.neo.Header;
